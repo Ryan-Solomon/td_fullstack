54e0a01e2735db63b8fabc276eb493649717442e
Ryan <ryan@ryansolomon.io>
2021-07-29 06:07:48 -0600

    add todo model

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo {self.id} {self.description}>"
