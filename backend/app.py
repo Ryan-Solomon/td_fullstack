15d952b936e643f7d6138e8c03ad7340340f13ad
Ryan <ryan@ryansolomon.io>
2021-07-30 06:12:54 -0600

    backend api

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres//'
+# db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+# class Todo(db.Model):
+#     __tablename__ = 'todos'
+#     id = db.Column(db.Integer, primary_key=True)
+#     description = db.Column(db.String(), nullable=False)
 
-    def __repr__(self):
-        return f"<Todo {self.id} {self.description}>"
+#     def __repr__(self):
+#         return f"<Todo {self.id} {self.description}>"
+
+
+# db.create_all()
 
 
 @app.route('/')
 def index():
-    return jsonify(data=Todo.query.all())
+    return {'data': 'hey hey'}
