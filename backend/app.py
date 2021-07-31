c4583830edeaa125bc6f2e813c4548b93558edba
Ryan <ryan@ryansolomon.io>
2021-07-31 16:53:09 -0600

    pg

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,22 @@
 from flask import Flask
-from flask.json import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres//'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/todoapp'
+db = SQLAlchemy(app)
 
 
-# class Todo(db.Model):
-#     __tablename__ = 'todos'
-#     id = db.Column(db.Integer, primary_key=True)
-#     description = db.Column(db.String(), nullable=False)
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
 
-#     def __repr__(self):
-#         return f"<Todo {self.id} {self.description}>"
+    def __repr__(self):
+        return f"<Todo {self.id} {self.description}>"
 
 
-# db.create_all()
+db.create_all()
 
 
 @app.route('/data', methods=['GET'])
